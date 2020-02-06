481a550d94487c048f22a688e740906529902136
bennydrspc <bennydrm16@gmail.com>
2020-02-06 11:07:34 +0700

    database company.sql

--- company.sql
+++ company.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2019 at 05:24 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.8
+-- Generation Time: Feb 06, 2020 at 05:06 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -33,28 +33,21 @@ CREATE TABLE `absent` (
   `employee_id` int(11) NOT NULL,
   `present` int(11) NOT NULL,
   `permission` int(11) NOT NULL,
+  `sick` int(11) NOT NULL,
   `alpha` int(11) NOT NULL,
-  `total_absent` int(11) NOT NULL,
   `lembur` int(11) NOT NULL,
-  `month` int(11) NOT NULL
+  `month` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `absent`
 --
 
-INSERT INTO `absent` (`id`, `employee_id`, `present`, `permission`, `alpha`, `total_absent`, `lembur`, `month`) VALUES
-(12019, 1000, 5, 0, 3, 0, 3, 12018),
-(12020, 1001, 13, 0, 0, 0, 0, 12018),
-(12021, 1111, 6, 0, 0, 0, 0, 12018),
-(12022, 1234, 12, 30, 1, 0, 20, 12018),
-(12023, 1000, 0, 0, 0, 0, 0, 22018),
-(12024, 1001, 0, 0, 0, 0, 0, 22018),
-(12025, 1002, 0, 0, 0, 0, 0, 22018),
-(12026, 1003, 0, 0, 0, 0, 0, 22018),
-(12027, 1004, 0, 0, 0, 0, 0, 22018),
-(12028, 1111, 0, 0, 0, 0, 0, 22018),
-(12029, 1234, 0, 0, 0, 0, 0, 22018);
+INSERT INTO `absent` (`id`, `employee_id`, `present`, `permission`, `sick`, `alpha`, `lembur`, `month`) VALUES
+(12081, 11111, 24, 0, 0, 0, 0, '2020-02'),
+(12082, 22222, 15, 2, 1, 1, 0, '2020-02'),
+(12083, 11111, 24, 1, 1, 0, 750, '2020-01'),
+(12084, 22222, 21, 2, 0, 2, 750, '2020-01');
 
 -- --------------------------------------------------------
 
@@ -63,7 +56,9 @@ INSERT INTO `absent` (`id`, `employee_id`, `present`, `permission`, `alpha`, `to
 --
 
 CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
   `e_id_number` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `birth_place` varchar(128) NOT NULL,
   `birth_date` date NOT NULL,
@@ -73,7 +68,7 @@ CREATE TABLE `employee` (
   `no_phone` varchar(12) NOT NULL,
   `address` text NOT NULL,
   `religion` varchar(128) NOT NULL,
-  `no_npwp` varchar(25) NOT NULL,
+  `no_npwp` varchar(16) NOT NULL,
   `position_id` int(11) NOT NULL,
   `group_id` int(11) NOT NULL,
   `date_join` date NOT NULL,
@@ -85,14 +80,9 @@ CREATE TABLE `employee` (
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`e_id_number`, `name`, `birth_place`, `birth_date`, `sex`, `status`, `email`, `no_phone`, `address`, `religion`, `no_npwp`, `position_id`, `group_id`, `date_join`, `academic`, `image`) VALUES
-(1000, 'Arif', 'Bogor', '2019-09-01', 'Male', 'Single', 'arif@gmail.com', '08989898989', 'bogor', 'Islam', '344555', 2, 0, '2019-09-01', 'S1', '38a7cb0e5db0411e81fcfba0da22e1d71.jpg'),
-(1001, 'Samsul', 'Jakarta', '2001-01-01', 'Male', 'Single', 'samsul@gmail.com', '084444443333', 'Jakarta', 'Islam', '56565666666666666', 5, 0, '2019-09-09', 'S2', 'default.jpg'),
-(1002, 'Yondu', 'Hala', '1991-01-01', 'Male', 'Married', 'yondu@gmail.com', '08123345556', 'Jakarta barat', 'Islam', '98880122', 4, 2, '2019-09-14', 'S2', 'default.jpg'),
-(1003, 'Santoso', 'Jakarta', '1996-09-01', 'Male', 'Single', 'santoso@gmail.com', '08797979979', 'bintaro, tangerang', 'Islam', '123456789', 5, 2, '2019-09-14', 'S1', 'default.jpg'),
-(1004, 'Kuda', 'Jakarta', '0000-00-00', 'Female', 'Single', 'kuda@gmail.com', '088888888888', 'arab', 'Islam', '098765432345', 14, 2, '2019-09-15', 'S2', 'default.jpg'),
-(1111, 'Apri', 'Bogor', '0000-00-00', 'Female', 'Married', 'bedas_16@yahoo.co.id', '856666666', 'arab', 'Islam', '344555', 1, 1, '2019-09-01', 'S1', 'default.jpg'),
-(1234, 'Afkar', 'Bogor', '2018-01-06', 'Male', 'Single', 'afkar@gmail.com', '085777777777', 'Lebakwangi girang', 'Islam', '1111111111122222', 4, 2, '2019-09-01', 'S1 Akuntansi Universitas Indonesia', '88c85728a88449638242b7968346baf1.jpg');
+INSERT INTO `employee` (`id`, `e_id_number`, `user_id`, `name`, `birth_place`, `birth_date`, `sex`, `status`, `email`, `no_phone`, `address`, `religion`, `no_npwp`, `position_id`, `group_id`, `date_join`, `academic`, `image`) VALUES
+(16, 11111, 37, 'Apri', 'Bogor', '2003-04-01', 'Perempuan', 'Belum Menikah', 'hgs@jdhd.com', '089999999999', 'bogor', 'Islam', '093938973879387', 10, 2, '2020-02-04', 'S1', 'default.jpg'),
+(17, 22222, 38, 'Udin', 'Bogor', '1995-01-01', 'Laki-Laki', 'Belum Menikah', 'udin@jdhd.com', '082626262222', 'bogor', 'Islam', '084646357634343', 4, 2, '2020-02-04', 'S1', 'default.jpg');
 
 -- --------------------------------------------------------
 
@@ -118,6 +108,31 @@ INSERT INTO `group` (`id`, `name_group`, `tj_married`, `meal`, `overtime`, `alph
 (1, 'A1', 100000, 20000, 50000, 200000, 150000),
 (2, 'A2', 100000, 15000, 100000, 200000, 100000),
 (4, 'A3', 90000, 120000, 130000, 100000, 80000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `information`
+--
+
+CREATE TABLE `information` (
+  `id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `content` text NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `information`
+--
+
+INSERT INTO `information` (`id`, `title`, `content`, `user_id`, `updated_by`, `created_at`, `updated_at`) VALUES
+(4, 'Pengumuman libur', '<p>besok <strong>libur</strong></p>', 2, 0, '2020-01-30 02:58:35', '0000-00-00 00:00:00'),
+(6, 'Informasi', '<p>Note that here too the limited string contains more than 10 chars but since CI tries to maintain word integrity, it does not break the last word.</p>', 2, 0, '2020-01-30 03:25:11', '0000-00-00 00:00:00'),
+(7, 'Pengumuman', '<p>beselkjdd dkdkd dddddd</p>', 2, 2, '2020-01-31 03:45:09', '2020-01-31 10:47:24');
 
 -- --------------------------------------------------------
 
@@ -150,7 +165,7 @@ INSERT INTO `leave` (`id`, `employee_id`, `date`, `total_days`, `date_start`, `d
 --
 
 CREATE TABLE `position` (
-  `id` int(11) NOT NULL,
+  `id_position` int(11) NOT NULL,
   `name_position` varchar(255) NOT NULL,
   `salary` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -159,11 +174,11 @@ CREATE TABLE `position` (
 -- Dumping data for table `position`
 --
 
-INSERT INTO `position` (`id`, `name_position`, `salary`) VALUES
+INSERT INTO `position` (`id_position`, `name_position`, `salary`) VALUES
 (1, 'Director', 10000000),
 (2, 'Manager', 6000000),
 (4, 'Finance', 5500000),
-(5, 'HDR', 5500000),
+(5, 'HRD', 5500000),
 (10, 'Administrator', 5000000),
 (14, 'Secretary', 4500000);
 
@@ -178,25 +193,19 @@ CREATE TABLE `user` (
   `e_id_number` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
-  `image` varchar(128) NOT NULL,
   `password` varchar(256) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
-  `date_created` int(11) NOT NULL
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `e_id_number`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
-(2, 1111, 'apri', 'bedas_16@yahoo.co.id', '38a7cb0e5db0411e81fcfba0da22e1d7.jpg', '$2y$10$4SQCn9UcQhmiuMkt32QwseAJcwY7JwKYX0PxqXvPdocw4J3zNgTCO', 1, 1, 1566816535),
-(17, 1000, 'Arif', 'arif@gmail.com', '', '$2y$10$qvXci10lyzBJvnq8280zJuN8d2iKSNU33ATRx93/ym3m.y.kAZM16', 2, 0, 0),
-(19, 1234, 'Afkar', 'afkar@gmail.com', '', '$2y$10$GIjvlJTtlrHF5e3VWYfORuGftpd1qF4uwXAOUZqXhngBt1ac2mtgy', 2, 0, 0),
-(20, 1001, 'Samsul', 'samsul@gmail.com', '', '$2y$10$uu4uE.TwZztcuov2rNuoteFr5bCUSstFTyMyZQATp2zA6IFHhW6J6', 2, 0, 0),
-(21, 1002, 'Yondu', 'yondu@gmail.com', '', '$2y$10$SXVIm4Ax9U0Kwrlv8xuUdOUKMs0w4asZKh/v21BAeYUrvXB.MbcIm', 2, 0, 0),
-(22, 1003, 'Santoso', 'santoso@gmail.com', '', '$2y$10$ovzI5dKLHyz2Rm/g/VKwKOpdbmtL7Vx4K6Q3RlS8R4jDxD.1vim9y', 2, 0, 0),
-(23, 1004, 'Kuda', 'kuda@gmail.com', '', '$2y$10$0rpk/YfVkf22P6bpEP4xoueUYGupE6wCgJhrPAmMB1vF/4.mwuhPO', 2, 0, 0);
+INSERT INTO `user` (`id`, `e_id_number`, `name`, `email`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(37, 11111, 'Apri', 'hgs@jdhd.com', '$2y$10$rBb/A0NJBcjn8.LwqB.Q0.ecsWNrNQlcrX9pcIh4oWhouF8IKtMKq', 1, 1, '2020-02-04 05:54:18'),
+(38, 22222, 'Udin', 'udin@jdhd.com', '$2y$10$sCPeXBsTXRu2yPvBqauEfu3bO6Y7N8r.pV7vt7MPs1v7CEm2DPEai', 2, 1, '2020-02-04 10:02:40');
 
 -- --------------------------------------------------------
 
@@ -220,7 +229,8 @@ INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
 (3, 2, 2),
 (6, 1, 3),
 (7, 1, 6),
-(8, 1, 8);
+(8, 1, 8),
+(11, 1, 9);
 
 -- --------------------------------------------------------
 
@@ -241,7 +251,8 @@ INSERT INTO `user_menu` (`id`, `menu`) VALUES
 (1, 'Admin'),
 (2, 'User'),
 (3, 'Menu'),
-(6, 'Employee');
+(6, 'Employee'),
+(9, 'Info');
 
 -- --------------------------------------------------------
 
@@ -274,6 +285,7 @@ CREATE TABLE `user_sub_menu` (
   `title` varchar(128) NOT NULL,
   `url` varchar(128) NOT NULL,
   `icon` varchar(128) NOT NULL,
+  `order_by` int(11) NOT NULL,
   `is_active` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -281,21 +293,23 @@ CREATE TABLE `user_sub_menu` (
 -- Dumping data for table `user_sub_menu`
 --
 
-INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
-(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
-(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user', 1),
-(3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
-(4, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 1),
-(5, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 1),
-(6, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
-(7, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
-(8, 6, 'Employee', 'employee', 'fas fa-fw fa-users', 1),
-(9, 6, 'Position', 'employee/position', 'fas fa-fw fa-briefcase', 1),
-(12, 8, 'cobalah', 'coba', 'fas fa-fw fa-user', 1),
-(13, 6, 'Leave', 'employee/leave', 'fas fa-fw fa-calendar-day', 1),
-(14, 6, 'Absent', 'employee/absent', 'fas fa-fw fa-clipboard-check', 1),
-(15, 6, 'Salary', 'employee/salary', 'fas fa-fw fa-credit-card', 1),
-(16, 6, 'Group', 'employee/group', 'fas fa-fw fa-users-cog', 1);
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `order_by`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1, 1),
+(2, 2, 'My Profile', 'user/profil', 'fas fa-fw fa-user', 4, 1),
+(3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 5, 0),
+(4, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder', 7, 1),
+(5, 3, 'Submenu Management', 'menu/submenu', 'fas fa-fw fa-folder-open', 8, 1),
+(6, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 2, 1),
+(7, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 6, 1),
+(8, 6, 'Pegawai', 'employee', 'fas fa-fw fa-users', 9, 1),
+(9, 6, 'Jabatan', 'employee/position', 'fas fa-fw fa-briefcase', 14, 1),
+(12, 8, 'cobalah', 'coba', 'fas fa-fw fa-user', 0, 1),
+(13, 6, 'Cuti', 'employee/leave', 'fas fa-fw fa-calendar-day', 12, 1),
+(14, 6, 'Absen', 'employee/absent', 'fas fa-fw fa-clipboard-check', 10, 1),
+(15, 6, 'Gaji', 'employee/salary', 'fas fa-fw fa-credit-card', 11, 1),
+(16, 6, 'Golongan', 'employee/group', 'fas fa-fw fa-users-cog', 13, 1),
+(18, 9, 'Informasi', 'info', 'fas fa-fw fa-info', 15, 1),
+(19, 2, 'Home', 'user', 'fas fa-fw fa-home', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -333,12 +347,19 @@ ALTER TABLE `absent`
 -- Indexes for table `employee`
 --
 ALTER TABLE `employee`
-  ADD PRIMARY KEY (`e_id_number`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `position_id` (`position_id`);
 
 --
 -- Indexes for table `group`
 --
 ALTER TABLE `group`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `information`
+--
+ALTER TABLE `information`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -351,7 +372,7 @@ ALTER TABLE `leave`
 -- Indexes for table `position`
 --
 ALTER TABLE `position`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_position`);
 
 --
 -- Indexes for table `user`
@@ -397,13 +418,25 @@ ALTER TABLE `user_token`
 -- AUTO_INCREMENT for table `absent`
 --
 ALTER TABLE `absent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12030;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12085;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `group`
 --
 ALTER TABLE `group`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `information`
+--
+ALTER TABLE `information`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `leave`
@@ -415,25 +448,25 @@ ALTER TABLE `leave`
 -- AUTO_INCREMENT for table `position`
 --
 ALTER TABLE `position`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_position` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `user_access_menu`
 --
 ALTER TABLE `user_access_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `user_menu`
 --
 ALTER TABLE `user_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `user_role`
@@ -445,13 +478,23 @@ ALTER TABLE `user_role`
 -- AUTO_INCREMENT for table `user_sub_menu`
 --
 ALTER TABLE `user_sub_menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `user_token`
 --
 ALTER TABLE `user_token`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `position` (`id_position`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
